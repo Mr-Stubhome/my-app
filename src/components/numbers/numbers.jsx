65e1d88b374321c428a67ba2d09bdc3e0cad943c
Stubhome <@dmitrijkofman2>
2025-03-11 23:53:45 +0600

    useState counter

--- src/components/numbers/numbers.jsx
+++ src/components/numbers/numbers.jsx
@@ -1,55 +1,47 @@
-import { Component } from "react";
-import Counter from "../counter/counter";
+import { useState } from "react";
+import { Counter } from "../counter/counter";
 import "../../components/styles.css";
 
-class Numbers extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      countedNumber: 0,
-      numberArray: [],
-    };
-  }
+export function Numbers() {
+  const [countedNumber, setCountedNumber] = useState(0);
+  const [numberArray, setNumberArray] = useState([]);
 
-  addNumber = (num) => {
-    this.setState((prevState) => {
-      const newArray = [...prevState.numberArray, num];
+  const addNumber = (num) => {
+    setNumberArray((prevArray) => {
+      const newArray = [...prevArray, num];
       const newCount = parseInt(newArray.join(""), 10); // Преобразуем в число
-      return { numberArray: newArray, countedNumber: newCount };
+      setCountedNumber(newCount);
+      return newArray;
     });
   };
 
-  reset = () => {
-    this.setState({ countedNumber: 0, numberArray: [] });
+  const handleClickReset = () => {
+    setNumberArray([]);
+    setCountedNumber(0);
   };
 
-  render() {
-    console.log(this.state.countedNumber);
-    return (
-      <>
-        <div className="main">
-          <Counter countedNumber={this.state.countedNumber} />
-          <div className="wrapperNumbers">
-            <h1>Рабочее число: {this.state.countedNumber}</h1>
-            <div className="numbersBlock">
-              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => (
-                <button
-                  key={num}
-                  className="numbers"
-                  onClick={() => this.addNumber(num)}
-                >
-                  {num}
-                </button>
-              ))}
-              <button className="reset" onClick={this.reset}>
-                Сбросить
+  return (
+    <>
+      <div className="main">
+        <Counter countedNumber={countedNumber} />
+        <div className="wrapperNumbers">
+          <h1>Рабочее число: {countedNumber}</h1>
+          <div className="numbersBlock">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((num) => (
+              <button
+                key={num}
+                className="numbers"
+                onClick={() => addNumber(num)}
+              >
+                {num}
               </button>
-            </div>
+            ))}
+            <button className="reset" onClick={handleClickReset}>
+              Сбросить
+            </button>
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
-
-export default Numbers;
